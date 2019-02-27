431b5cb03cec4469ff3d0ab9faa40ea85d995734
thedevilswaffle <thedevilswaffle@gmail.com>
2019-02-26 16:47:20 -0800

    added leantween, updated player controller (broken), added doors

---
+++ testing/Assets/PlayerSensor.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+
+public enum DoorStatus
+{
+    OPEN,
+    CLOSE
+};
+public class PlayerSensor : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    [SerializeField] DoorStatus status;
+    [SerializeField] Vector3 offset;
+    [SerializeField] AnimationCurve openCurve;
+    [SerializeField] float openTime;
+    [SerializeField] AnimationCurve closeCurve;
+    [SerializeField] float closeTime;
+    [SerializeField] string[] targets;
+
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// called when the script instance is being loaded.
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void Awake()
+    {
+        UpdateDoorStatus(status);
+    }
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// called before the first frame update
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void Start()
+    {
+        
+    }
+    #region TRIGGERS
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="_other">The other Collider involved in this collision.</param>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void OnTriggerEnter(Collider _other)
+    {
+        for(int _index = 0; _index < targets.Length; ++_index)
+        {
+            if(_other.gameObject.tag == targets[_index])
+            {
+                print(_other.gameObject.name + " detected! open this door!");
+                OpenDoor();
+            }
+        }
+    }
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other exits the trigger.
+    /// </summary>
+    /// <param name="_other">The other Collider involved in this collision.</param>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void OnTriggerExit(Collider _other)
+    {
+        for(int _index = 0; _index < targets.Length; ++_index)
+        {
+            if(_other.gameObject.tag == targets[_index])
+            {
+                print(_other.gameObject.name + " detected! close this door!");
+                CloseDoor();
+            }
+        }
+    }
+    #endregion
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// determine what to do based on door's status
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void UpdateDoorStatus(DoorStatus _status)
+    {
+        status = _status;
+
+        if(_status == DoorStatus.CLOSE)
+        {
+            CloseDoor();
+        }
+        else if (_status == DoorStatus.OPEN)
+        {
+            OpenDoor();
+        }
+    }
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// description
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void CloseDoor()
+    {
+        //door.transform.localPosition = Vector3.zero;
+        LeanTween.moveLocal(door, Vector3.zero, closeTime).setEase(closeCurve);
+    }
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// description
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void OpenDoor()
+    {
+        //door.transform.localPosition += offset;
+        LeanTween.moveLocal(door, offset, openTime).setEase(openCurve);
+    }
+}
--- testing/Assets/Scripts/TestController.cs
+++ testing/Assets/Scripts/TestController.cs
@@ -6,10 +6,18 @@ public class TestController : MonoBehaviour
 {
     [SerializeField] Transform cam;
 
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float maxSpeed = 10f;
+    [SerializeField] AnimationCurve accelerationCurve;
+    [SerializeField] float timeToMaxSpeed = 2f;
+    [SerializeField] AnimationCurve decelerationCurve;
+    [SerializeField] float timeToZeroSpeed = 0.5f;
     [SerializeField] float turnSpeed = 10f;
     Quaternion targetRotation;
+    Rigidbody rb;
 
+    float currentSpeed = 0;
+    float accelerationTimer = 0;
+    float decelerationTimer = 0;
     float angle;
     Vector2 input;
     
@@ -29,7 +37,7 @@ public class TestController : MonoBehaviour
     /*///////////////////////////////////////////////////////////////////////////////////////////////*/
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
     /*////////////////////////////////////////////////////////////////////////////////////////////////*/
     /// <summary>
@@ -87,7 +95,43 @@ public class TestController : MonoBehaviour
     /*///////////////////////////////////////////////////////////////////////////////////////////////*/
     void Move()
     {
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        //transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        rb.velocity = transform.forward * currentSpeed;
+        //rb.AddForce(transform.forward * moveSpeed * Time.deltaTime);
+        print(transform.forward);
+    }
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// description
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void CalculateCurrentSpeed()
+    {
+        if(input.y == 1 )
+        {
+            print("accelerating");
+            decelerationTimer = 0;
+            accelerationTimer += Time.deltaTime;
+            currentSpeed = maxSpeed * accelerationCurve.Evaluate(accelerationTimer/timeToMaxSpeed);
+        }
+        else if(Mathf.Abs(input.y) < 1)
+        {
+            print("decelerating");
+            accelerationTimer = 0;
+            decelerationTimer += Time.deltaTime;
+
+            currentSpeed -= decelerationCurve.Evaluate(decelerationTimer/timeToZeroSpeed);
+        }
+        if(currentSpeed > maxSpeed)
+        {
+            print("max speed achieved");
+            currentSpeed = maxSpeed;
+        }
+        else if(currentSpeed < 0)
+        {
+            print("zero speed");
+            currentSpeed = 0;
+        }
     }
     /*////////////////////////////////////////////////////////////////////////////////////////////////*/
     /// <summary>
@@ -97,7 +141,16 @@ public class TestController : MonoBehaviour
     void Update()
     {
         UpdateInput();
-        print(input);
+        CalculateCurrentSpeed();
+    }
+    /*////////////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////////*/
+    void FixedUpdate()
+    {
+        //no input? exit the update loop
         if(Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
         CalculateDirection();
         Rotate();
