dc92d72f4519f9f42315e9aade1548eea3177603
thedevilswaffle <thedevilswaffle@gmail.com>
2019-04-01 15:57:42 -0700

    trying out an a* tut to auto attach powered objects

---
+++ testing/Assets/Scripts/astar_example/Grid.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Transform startPosition;
+    public LayerMask wallMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public float distance;
+
+    Node[,] grid;
+    public List<Node> finalPath;
+
+    float nodeDiameter;
+    int gridSizeX;
+    int gridSizeY;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);    
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);   
+
+        CreateGrid(); 
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+        for(int _y = 0; _y < gridSizeY; ++_y)
+        {
+            for(int _x = 0; _x < gridSizeX; ++_x)
+            {
+                Vector3 worldPoint = bottomLeft + Vector3.right * (_x * nodeDiameter - nodeRadius) + Vector3.forward * (_y * nodeDiameter + nodeRadius);
+                bool wall = true;
+                if (Physics.CheckSphere(worldPoint, nodeRadius, wallMask))
+                {
+                    wall = false;
+                }
+
+                grid[_y, _x] = new Node(wall, worldPoint, _x, _y);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if(grid != null)
+        {
+            foreach (Node _node in grid)
+            {
+                if(_node.isWall)
+                {
+                    Gizmos.color = Color.white;
+                }
+                else
+                {
+                    Gizmos.color = Color.yellow;
+                }
+                if (finalPath != null)
+                {
+                    Gizmos.color = Color.red;
+                }
+
+                Gizmos.DrawCube(_node.position, Vector3.one * (nodeDiameter - distance));
+            }
+        }
+    }
+}
---
+++ testing/Assets/Scripts/astar_example/MoveTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    public LayerMask hitLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouse = Input.mousePosition; //mouse pos
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse); //cast a ray to mouse pos
+            RaycastHit hit; // used to store where the ray hit
+            if(Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers)) //make sure we don't hit walls
+            {
+                this.transform.position = hit.point; //move the target to the mouse position
+            }
+        }
+    }
+}
