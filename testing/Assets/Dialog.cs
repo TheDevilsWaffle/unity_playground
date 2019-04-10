d5dc09bcaeba4c1c9cfd8df0eb769723d586503a
thedevilswaffle <thedevilswaffle@gmail.com>
2019-04-10 16:29:13 -0700

    start of dialog system, need to position world space -> screen space

---
+++ testing/Assets/Dialog.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : Interactable
+{
+    #region PROPERTIES
+    [SerializeField] DialogSystem dialogSystem;
+    [TextArea(3, 5)]
+    [SerializeField] List<string> dialogs = new List<string>();
+    [SerializeField] Vector3 offsetPosition = Vector3.zero;
+
+    Vector3 position;
+    int dialogCount;
+    int currentDialog = 0;
+    bool isDialogActive = false;
+    #endregion
+
+    #region INITIALIZATION
+    /// <summary>
+    /// Called when the script is loaded or a value is changed in the
+    /// inspector (Called in the editor only).
+    /// </summary>
+    void OnValidate()
+    {
+        dialogSystem = FindObjectOfType<DialogSystem>();
+    }
+    /*///////////////////////////////////////////////////////////////////////////////////////////*/
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    /*///////////////////////////////////////////////////////////////////////////////////////////*/
+    void Awake()
+    {
+        ResetDialog();
+    }
+    #endregion
+    #region METHODS
+    /*///////////////////////////////////////////////////////////////////////////////////////////*/
+    /// ResetDialog()
+    /*///////////////////////////////////////////////////////////////////////////////////////////*/
+    void ResetDialog()
+    {
+        position = Camera.main.WorldToScreenPoint(transform.position) + offsetPosition;
+        dialogCount = dialogs.Count;
+        currentDialog = 0;
+        isDialogActive = false;
+    }
+    #endregion
+    #region OVERRIDE METHODS
+    public override void Activate()
+    {
+        dialogSystem.DisplayDialog(this.gameObject.name, dialogs[0], position);
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Activate();
+        }
+    }
+    #endregion
+}
---
+++ testing/Assets/DialogSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogSystem : MonoBehaviour
+{
+    #region PROPERTIES
+    [SerializeField] GameObject dialogContainer;
+    #endregion
+    public void DisplayDialog(string _name, string _dialog, Vector3 _position)
+    {
+        //turn on dialog if it needs to be turned on
+        ActivateDialog(_position);
+    }
+
+    void ActivateDialog(Vector3 _position)
+    {
+        if (dialogContainer.activeSelf)
+            return;
+        else
+        {
+            dialogContainer.SetActive(true);
+            dialogContainer.GetComponent<RectTransform>().position = _position;
+        }
+    }
+}
